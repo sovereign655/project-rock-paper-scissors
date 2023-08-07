9e57fbf57f2e36dd186d815bb19399ccec9ab4c3
sovereign655 <140292187+sovereign655@users.noreply.github.com>
2023-08-07 12:52:34 -0400

    Completed the functional logic of the game to work independently in the browser through the console.

--- README.md
+++ README.md
@@ -7,4 +7,34 @@ This is one of my first JavaScript program built from scratch.
 It is meant to demonstrate my ability to plan out a functioning application, using most, if not all, methods previously learned.
 to find out more about The Odin Project, visit this website: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
+CC is short for "Computer controlled player"
+step 1
+how rock-paper-scissors is played:
+
+    two players agree and begin to play,
+
+    they decide if they are going to show 1 of three options:
+    rock
+    paper
+    scissors
+
+    from either perspective:
+    if rock is played:
+
+        it will win scissors is played.
+        it will lose if paper is played.
+    
+    if paper is played:
+
+        it will win if rock is played.
+        it will lose if scissors is played.
+    
+    if scissors is played:
+
+        it will win if paper is played.
+        it will lose if rock is played.
+    
+    if both players close the same option, it is a draw and a
+    reroll can be decided.
+
 ...
--- RPS.js
+++ RPS.js
@@ -1,36 +1,3 @@
-// console.log("hello from console");
-
-/*
-CC is short for "Computer controlled player"
-step 1
-how rock-paper-scissors is played:
-
-    two players agree and begin to play,
-
-    they decide if they are going to show 1 of three options:
-    rock
-    paper
-    scissors
-
-    from either perspective:
-    if rock is played {
-        it will win scissors is played.
-        it will lose if paper is played.
-    }
-    if paper is played {
-        it will win if rock is played.
-        it will lose if scissors is played.
-    }
-    if scissors is played {
-        it will win if paper is played.
-        it will lose if rock is played.
-    }
-
-    if both players close the same option, it is a draw and a
-    reroll can be decided.
-
-*/
-
 let player, CC;
 
 
@@ -53,17 +20,35 @@ const letComputerChoice = () => {
 
     if (Selector === 0){
         Option = "rock";
-    } else if (Selector === 1){
-        Option = 'paper'
+    } else if (Selector === 1) {
+        Option = "paper";
     } else if (Selector === 2) {
-        Option = 'scissors'
+        Option = "scissors";
     }
 
-    console.log(Option);
     return Option;
 
 
 }
 
-playerSelection();
-letComputerChoice();
+const play_RPS = () => {
+    let result;
+    let computerChoice = letComputerChoice();
+    let playerResult = playerSelection();
+
+    if (computerChoice === playerResult) {
+        result =  "draw!";
+
+    } else if (playerResult === "rock" && computerChoice === "scissors" || playerResult === "scissors" && computerChoice === "paper" || playerResult === "paper" && computerChoice === "rock") {
+        result = "you won";
+        
+    } else if (playerResult === "scissors" && computerChoice === "rock" || playerResult === "paper" && computerChoice === "scissors" || playerResult === "rock" && computerChoice === "paper") {
+        result = "you lost";
+    }
+
+    console.log(computerChoice);
+    console.log(result)   
+
+}
+
+play_RPS();
